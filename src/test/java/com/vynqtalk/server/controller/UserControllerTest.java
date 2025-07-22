fd40fe79080835949f8cffdf1d227ba02b903b6a
goal651 <bugiriwilson651@gmail.com>
2025-07-22 23:58:59 +0200

    refactor: update import statement in UserControllerTest to use user-specific UserService for improved clarity and organization

--- src/test/java/com/vynqtalk/server/controller/UserControllerTest.java
+++ src/test/java/com/vynqtalk/server/controller/UserControllerTest.java
@@ -2,7 +2,8 @@ package com.vynqtalk.server.controller;
 
 import com.vynqtalk.server.controller.user.UserController;
 import com.vynqtalk.server.mapper.UserMapper;
-import com.vynqtalk.server.service.UserService;
+import com.vynqtalk.server.service.user.UserService;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
