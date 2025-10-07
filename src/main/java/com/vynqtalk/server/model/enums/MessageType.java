8dfc2cc9bb891eda17bf54b410aa8abd4b6c1117
goal651 <bugiriwilson651@gmail.com>
2025-10-07 06:24:55 +0200

    fixed sending group messages

--- src/main/java/com/vynqtalk/server/model/enums/MessageType.java
+++ src/main/java/com/vynqtalk/server/model/enums/MessageType.java
@@ -1,9 +1,9 @@
 package com.vynqtalk.server.model.enums;
 
 public enum MessageType {
-    text,
-    image,
-    audio,
-    video,
-    file
+    TEXT,
+    IMAGE,
+    AUDIO,
+    VIDEO,
+    FILE
 }
